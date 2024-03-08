49ee6eff1f129910d0b2b810165bbac2babb53f1
rikuyaosawa <rikuyaosawa@gmail.com>
2024-03-08 18:34:06 +0900

    complete API project

--- 02 Backend/00 Projects/05 API Project/index.js
+++ 02 Backend/00 Projects/05 API Project/index.js
@@ -41,14 +41,60 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
 
 //CHALLENGE 2: GET a specific post by id
+app.get("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = posts.findIndex((post) => (post.id === id));
+  const foundPost = posts[searchIndex];
+  console.log(foundPost);
+  res.json(foundPost);
+});
 
 //CHALLENGE 3: POST a new post
+app.post("/posts", (req, res) => {
+  const newPost = {
+    id : posts.length + 1,
+    title : req.body.title,
+    content : req.body.content,
+    author : req.body.author,
+    date : new Date(),
+  };
+  console.log(newPost);
+  posts.push(newPost);
+  res.json(newPost);
+});
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingPost = posts.find((post) => post.id === id);
+  const updatedPost = {
+    id : id,
+    title : req.body.title || existingPost.title,
+    content : req.body.content || existingPost.content,
+    author : req.body.author || existingPost.author,
+    date : new Date(),
+  };
+  
+  const searchIndex = posts.findIndex((post) => post.id === id);
+  posts[searchIndex] = updatedPost;
+  res.json(posts[searchIndex]);
+});
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  const searchIndex = posts.findIndex((post) => post.id === id);
+  if (id > -1) {
+    posts.splice(searchIndex, 1);
+    res.json({message : "Post deleted."});
+  };
+});
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
--- 02 Backend/00 Projects/05 API Project/server.js
+++ 02 Backend/00 Projects/05 API Project/server.js
@@ -15,7 +15,6 @@ app.use(bodyParser.json());
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(`${API_URL}/posts`);
-    console.log(response);
     res.render("index.ejs", { posts: response.data });
   } catch (error) {
     res.status(500).json({ message: "Error fetching posts" });
@@ -30,7 +29,6 @@ app.get("/new", (req, res) => {
 app.get("/edit/:id", async (req, res) => {
   try {
     const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
-    console.log(response.data);
     res.render("modify.ejs", {
       heading: "Edit Post",
       submit: "Update Post",
@@ -45,7 +43,6 @@ app.get("/edit/:id", async (req, res) => {
 app.post("/api/posts", async (req, res) => {
   try {
     const response = await axios.post(`${API_URL}/posts`, req.body);
-    console.log(response.data);
     res.redirect("/");
   } catch (error) {
     res.status(500).json({ message: "Error creating post" });
@@ -60,7 +57,6 @@ app.post("/api/posts/:id", async (req, res) => {
       `${API_URL}/posts/${req.params.id}`,
       req.body
     );
-    console.log(response.data);
     res.redirect("/");
   } catch (error) {
     res.status(500).json({ message: "Error updating post" });
@@ -70,10 +66,11 @@ app.post("/api/posts/:id", async (req, res) => {
 // Delete a post
 app.get("/api/posts/delete/:id", async (req, res) => {
   try {
+    console.log("Hi");
     await axios.delete(`${API_URL}/posts/${req.params.id}`);
     res.redirect("/");
   } catch (error) {
-    res.status(500).json({ message: "Error deleting post" });
+    res.status(500).json({ message: error });
   }
 });
 
