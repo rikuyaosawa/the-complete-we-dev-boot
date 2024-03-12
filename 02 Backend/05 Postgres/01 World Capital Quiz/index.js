3f6a9dc27ce119eccc0c6fccda147394b000f7ad
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-03-12 15:44:55 +0900

    connect db in World Capital Quiz

--- 02 Backend/05 Postgres/01 World Capital Quiz/index.js
+++ 02 Backend/05 Postgres/01 World Capital Quiz/index.js
@@ -1,14 +1,34 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-let quiz = [
-  { country: "France", capital: "Paris" },
-  { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
-];
+const db = new pg.Client({
+  user : "postgres",
+  host : "localhost",
+  database : "world",
+  password : "postgres",
+  port : 5433,
+});
+
+db.connect();
+
+let quiz = [{
+  country : "France",
+  capital : "Paris",
+}];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    console.log("I am here.");
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
