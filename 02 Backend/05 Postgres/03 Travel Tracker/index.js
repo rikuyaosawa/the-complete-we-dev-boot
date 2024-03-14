45ec3fffab29a2105a19dce1530f2ba07cb30dd9
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-03-14 11:27:11 +0900

    update Postgres/Travel Tracker

--- 02 Backend/05 Postgres/03 Travel Tracker/index.js
+++ 02 Backend/05 Postgres/03 Travel Tracker/index.js
@@ -1,14 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import morgan from "morgan";
 
 const app = express();
 const port = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(morgan("dev"));
 
 const db = new pg.Client({
   user : "postgres",
@@ -20,22 +15,42 @@ const db = new pg.Client({
 
 db.connect();
 
-app.get("/", async (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
+
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
-  res.render("index.ejs", {
-    countries : countries,
-    total : countries.length
-  })
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
 });
 
-app.post("/add", (res, req) => {
-  total += 1;
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
